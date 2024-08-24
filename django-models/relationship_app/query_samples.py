88b9fd69064c23dfff6c10c093904c8df36d8c8f
Ahmed Medhat <149432254+AHMEDMEDHAT0408@users.noreply.github.com>
2024-08-24 06:54:23 +0300

    Fix query to filter books by author

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,10 +1,13 @@
-from .models import Author, Library
+from .models import Author, Book
 
-# Query all books by a specific author
 def get_books_by_author(author_name):
+    # Get the author object by name
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    
+    # Use objects.filter() to get all books by the specific author
+    books = Book.objects.filter(author=author)
 
+    # Print the titles of the books
     for book in books:
         print(book.title)
 
