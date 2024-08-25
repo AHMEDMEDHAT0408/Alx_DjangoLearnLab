6337e11e32125bfde8258a608cfe865d6268da77
Ahmed Medhat <149432254+AHMEDMEDHAT0408@users.noreply.github.com>
2024-08-25 10:31:10 +0300

    ya rab

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import list_books, LibraryDetailView  # Import both views
+from . import views
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # URL pattern for function-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL pattern for class-based view
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView  # Import DetailView
-from .models import Library, Book
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-# Class-based view to display details for a specific library
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
