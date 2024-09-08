7b079a51e21a468f7967862ec1f4135e2c1a38e6
Ahmed Medhat <149432254+AHMEDMEDHAT0408@users.noreply.github.com>
2024-09-09 00:51:36 +0300

    Update test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,75 +1,65 @@
 from django.test import TestCase
-from rest_framework.test import APIClient
 from rest_framework import status
-from django.urls import reverse
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 from .models import Book
+from .serializers import BookSerializer
 
 class BookAPITests(TestCase):
     def setUp(self):
+        # Create a user and authenticate
+        self.user = User.objects.create_user(username='testuser', password='testpass')
         self.client = APIClient()
-        self.book_data = {'title': 'Test Book', 'author': 'Author', 'description': 'A test book', 'price': 10.99}
-        self.url = reverse('book-list')
+        self.client.login(username='testuser', password='testpass')
 
-    # Test book creation
+        # Create some sample data
+        self.book1 = Book.objects.create(title='Book One', author='Author One', publication_year=2021, price=10.00)
+        self.book2 = Book.objects.create(title='Book Two', author='Author Two', publication_year=2022, price=15.00)
+        
+    def test_list_books(self):
+        response = self.client.get('/api/books/')
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
     def test_create_book(self):
-        response = self.client.post(self.url, self.book_data, format='json')
+        data = {
+            'title': 'Book Three',
+            'author': 'Author Three',
+            'publication_year': 2023,
+            'price': 20.00
+        }
+        response = self.client.post('/api/books/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['title'], self.book_data['title'])
+        self.assertEqual(Book.objects.count(), 3)
+        self.assertEqual(Book.objects.get(id=response.data['id']).title, 'Book Three')
 
-    # Test getting list of books
-    def test_get_books(self):
-        Book.objects.create(title="Test Book", author="Author", description="A test book", price=10.99)
-        response = self.client.get(self.url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertGreaterEqual(len(response.data), 1)
-
-    # Test retrieving a single book by ID
-    def test_get_book_detail(self):
-        book = Book.objects.create(title="Test Book", author="Author", description="A test book", price=10.99)
-        url = reverse('book-detail', kwargs={'pk': book.id})
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], book.title)
-
-    # Test updating a book
     def test_update_book(self):
-        book = Book.objects.create(title="Test Book", author="Author", description="A test book", price=10.99)
-        url = reverse('book-detail', kwargs={'pk': book.id})
-        updated_data = {'title': 'Updated Book', 'author': 'Updated Author', 'price': 15.99}
-        response = self.client.put(url, updated_data, format='json')
+        data = {'title': 'Updated Book One'}
+        response = self.client.patch(f'/api/books/{self.book1.id}/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Updated Book')
+        self.assertEqual(Book.objects.get(id=self.book1.id).title, 'Updated Book One')
 
-    # Test deleting a book
     def test_delete_book(self):
-        book = Book.objects.create(title="Test Book", author="Author", description="A test book", price=10.99)
-        url = reverse('book-detail', kwargs={'pk': book.id})
-        response = self.client.delete(url, format='json')
+        response = self.client.delete(f'/api/books/{self.book1.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(Book.objects.filter(id=book.id).exists())
+        self.assertEqual(Book.objects.count(), 1)
 
-    # Test filtering by author
-    def test_filter_books_by_author(self):
-        Book.objects.create(title="Book 1", author="Author A", description="Desc", price=10.99)
-        Book.objects.create(title="Book 2", author="Author B", description="Desc", price=12.99)
-        url = f'{self.url}?author=Author A'
-        response = self.client.get(url, format='json')
+    def test_filter_books(self):
+        response = self.client.get('/api/books/', {'author': 'Author Two'})
+        serializer = BookSerializer(Book.objects.filter(author='Author Two'), many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data, serializer.data)
 
-    # Test searching by title
     def test_search_books(self):
-        Book.objects.create(title="Searchable Book", author="Author", description="Desc", price=10.99)
-        url = f'{self.url}?search=Searchable'
-        response = self.client.get(url, format='json')
+        response = self.client.get('/api/books/', {'search': 'Book Two'})
+        serializer = BookSerializer(Book.objects.filter(title='Book Two'), many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertGreaterEqual(len(response.data), 1)
+        self.assertEqual(response.data, serializer.data)
 
-    # Test ordering by title
-    def test_order_books_by_title(self):
-        Book.objects.create(title="A Book", author="Author A", description="Desc", price=10.99)
-        Book.objects.create(title="Z Book", author="Author B", description="Desc", price=12.99)
-        url = f'{self.url}?ordering=title'
-        response = self.client.get(url, format='json')
+    def test_ordering_books(self):
+        response = self.client.get('/api/books/', {'ordering': 'title'})
+        serializer = BookSerializer(Book.objects.all().order_by('title'), many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data[0]['title'], "A Book")
+        self.assertEqual(response.data, serializer.data)
