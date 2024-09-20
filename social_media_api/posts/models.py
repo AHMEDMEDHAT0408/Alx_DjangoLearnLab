a3c481610dc429fd9b995fde0b1f8f470de2c4b5
Ahmed Medhat <149432254+AHMEDMEDHAT0408@users.noreply.github.com>
2024-09-20 04:10:49 +0300

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from accounts.models import CustomUser
 User = get_user_model()
 
 class Post(models.Model):
@@ -11,7 +11,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.content[:50]
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
